261ad67601b2d65a77c9708108bedee2f046e145
rtaha07 <rtaha07@gmail.com>
2020-07-16 21:01:59 -0400

    finished 1st spec

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,15 @@
 // YOUR CODE HERE
+class ${
+  constructor(string){
+    let str = string.substring(1);
+    if(string[0] === '.'){
+      this.elements = Array.from(document.getElementsByClassName(str));
+    }else if(string[0] === '#'){
+      this.elements = Array.from(document.getElementById(str));
+    }else{
+      this.elements = Array.from(document.getElementsByTagName(str));
+    }
+
+  }
+
+}
